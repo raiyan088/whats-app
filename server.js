743f403da44c6a895f0c0f71db563f04e8e8610a
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-08-23 16:07:22 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const WhatsApp = require('./whats-app-api');
+const express=require('express');
+
+const app = express();
 
 const whatsAppApi = new WhatsApp();
 
@@ -8,3 +11,11 @@ const whatsAppApi = new WhatsApp();
   }); 
 
 })()
+
+app.get('/send', function(req, res) {
+    
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening on port 3000...");
+});
